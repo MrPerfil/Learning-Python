faeaff679c40fd9f3e41e3479f9185140ba33e71
Perfil <MrPerfil@outlook.es>
2021-04-25 12:24:15 -0500

    Added NUMBERS guide !!!

---
+++ numbers.py
@@ -0,0 +1,13 @@
+##Numbers interactions! like adding, dividing, and other stuff
+one=("1")
+two=("2")
+three=(one + two)
+## u can add two texts (variables) and the "print" will just put them together!
+print (three)
+
+##And variables can be numbers, directly!!
+four=4
+five=5
+print (four + five)
+##and u can add numbers WITHOUT using variables
+print (6 + 7)
