3d8d11bcf72fb936e21c5ed84294f89e7e1905e8
Perfil <MrPerfil@outlook.es>
2021-04-25 18:36:43 -0500

    I updated the summary.

--- summary1.py
+++ summary1.py
@@ -7,7 +7,7 @@ print (1 + 2)
 ##This is a variable
 variable1=1
 variable2=2
-##You can add Variables if they are numbers
+##You can add Variables if they are plain numbers
 print (variable1 + variable2)
 ##Output should be 3, because 1+2=3
 ##You can also add text using variables, the text will just appear together
@@ -19,6 +19,20 @@ space=" "
 print (text1 + space + text2)
 ##You will get errors if you print a variable that does not exits.
 ##I won't do that because that would crash the entire script.
+
+##You can make numbers act like text, just adding ""
+numberistext1="1"
+numberistext2="2"
+print (numberistext1 + numberistext2)
+
+##You can also make aliases for the variables.
+##Frist make the original variable
+original="This is the text"
+##And then another variablem but the context of it is another variable.
+alias=original
+##And the you can print it using the name of the frist variable (original) or the alias.
+print (alias)
+print (original)
 
 ##This is a list
 mylist=[]
