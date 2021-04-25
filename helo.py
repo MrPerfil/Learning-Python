f71a24bfd4b9b4d261168ff93c52daa97b0ea674
Perfil <MrPerfil@outlook.es>
2021-04-25 11:22:02 -0500

    We added more data in the helo.py file
    Added data

---
+++ helo.py
@@ -0,0 +1,9 @@
+##This is the tipic "hello world" thing.
+print ("hello! I'm Perfil")
+text_number_one=("hello")
+print (text_number_one)
+text_A=("This is text A")
+text_B=("This is text B")
+
+print (text_A)
+print (text_B)
