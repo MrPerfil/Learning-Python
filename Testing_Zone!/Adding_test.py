f9087000913229b30f6b692f4546ecaef9b3983d
Perfil <MrPerfil@outlook.es>
2021-04-25 20:38:32 -0500

    Deleted Python Cache, added Testing Zone, added Adding_Test.py

---
+++ Testing_Zone!/Adding_test.py
@@ -0,0 +1,25 @@
+##Here im going to test adding variables and simple numbers.  Then adding lists and... That
+test1=1
+test2=2
+test3=3
+
+list1=[]
+list1.append(4)
+list1.append(5)
+list1.append(6)
+
+print (test1 + list1[2])
+##This was 6 + 1
+##Output was 7
+##I can add variables from a list and variables from nowhere = TRUE
+
+print (test1 + 1)
+##This was 1 + 1
+##Output was 2
+##I can add variables and simple numbers = TRUE
+
+text1="test"
+print (text1 + 1)
+##This was a TEXT + NUMBER
+##I get a error.
+##I can add text and numbers = FALSE
